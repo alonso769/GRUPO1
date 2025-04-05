29e8dcdebe08815545d53f310071428fb602272b
alonso769 <131563220+alonso769@users.noreply.github.com>
2025-04-04 19:48:08 -0500

    INGRESO DE CODIGO EN ALUMNO Y PRINCIPAL

--- GRUPO_1/src/clases/Alumno.java
+++ GRUPO_1/src/clases/Alumno.java
@@ -1,5 +1,15 @@
 package clases;
 
 public class Alumno {
+	//Atributos miembro con modificadores de acceso público
+	public String codigo;
+	public String nombre;
+	public int nota1;
+	public int nota2;
+	
+	//Métodos miembro
+	public double calcularPromedio() {
+		return (nota1 + nota2) / 2.0;
+	}
 
 }
--- GRUPO_1/src/gui/Principal.java
+++ GRUPO_1/src/gui/Principal.java
@@ -1,5 +1,34 @@
 package gui;
 
+import clases.Alumno;
+
 public class Principal {
 
+	public static void main(String[] args) {
+		//Declaración de variable del tipo de la clase
+		Alumno alumno1;
+		
+		//Construyendo el objeto en la variable
+		alumno1 = new Alumno();
+		
+		//Asignar valores a los atributos del objeto
+		alumno1.codigo = "N012587";
+		alumno1.nombre = "Pedro Navaja";
+		alumno1.nota1 = 17;
+		alumno1.nota2 = 9;
+		
+		//Segundo objeto
+		Alumno alumno2 = new Alumno();
+		alumno2.codigo = "N666666";
+		alumno2.nombre = "Juanito Alimaña";
+		alumno2.nota1 = 18;
+		alumno2.nota2 = 19;
+		
+		System.out.println("Alumno 1: " + alumno1);
+		System.out.println("Promedio alumno 1: " + alumno1.calcularPromedio());
+		System.out.println("Alumno 2: " + alumno2);
+		System.out.println("Promedio alumno 2: " + alumno2.calcularPromedio());
+	}
+
 }
+
